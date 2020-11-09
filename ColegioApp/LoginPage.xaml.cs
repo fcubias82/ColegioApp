39e3fa4c4e918e96d6ce4bc1f6ef3a8464e5cf3e
Víctor Fernando Cubías Cortez <victor.cubias@gmail.com>
2020-11-08 18:36:22 -0600

    Agregar archivos de proyecto.

---
+++ ColegioApp/LoginPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ColegioApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnIngresar_Clicked(object sender, EventArgs e)
+        {
+            if(txtUsuario.Text == "usuario" && txtContrasenia.Text == "123456")
+            {
+                Navigation.InsertPageBefore(new MainPage(),this);
+                await Navigation.PopAsync();                        
+            }
+            else
+            {
+                await DisplayAlert("Error", "Usuario ó contraseña incorrectos", "OK");
+            }
+        }
+    }
+}
---
+++ ColegioApp/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using ColegioApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ColegioApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            List<Alumno> ListaAlumnos = new List<Alumno>();
+            ListaAlumnos.Add(new Alumno
+            {
+                Codigo = "001",
+                Nombre = "Fernando",
+                Apellido = "Cubías"
+            });
+
+            ListaAlumnos.Add(new Alumno
+            {
+                Codigo = "002",
+                Nombre = "Juan",
+                Apellido = "Perez"
+            });
+
+            ListaAlumnos.Add(new Alumno
+            {
+                Codigo = "003",
+                Nombre = "Reyna",
+                Apellido = "Cortez"
+            });
+
+            lvAlumnos.ItemsSource = ListaAlumnos;
+
+        }
+    }
+}
